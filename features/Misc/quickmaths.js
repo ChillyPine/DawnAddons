dff41be3d93b99306843152e10757cc6ebce1cd0
ChillyPine <chillypines@gmail.com>
2025-10-18 12:51:24 -0600

    Made quick maths answer actually have a prefix

--- features/Misc/quickmaths.js
+++ features/Misc/quickmaths.js
@@ -1,17 +1,18 @@
 import settings from '../../config'
-;
+const prefix = "&5[Dawn&6Addons&5]"
+
 // Quick Maths Solver
 register("chat", (math) => {
     if (!settings().quickmathsprimalfear) return;
     const calculation = math.replaceAll(/(\x+)/g, "*").replace(/[^-()\d/*+.]/g, "");
     const answer = eval(calculation);
     setTimeout(() => {
-        ChatLib.chat(`&dQuick Maths Answer: &a&l${answer}`)
+        ChatLib.chat(`${prefix} &dQuick Maths Answer: &a&l${answer}`)
     }, 100); //Dont send message before it appears in chat
 
-    if (settings().autoanwserquickmaths) {
-        setTimeout(() => {
-            ChatLib.command('ac ${answer}') 
-        }, 200);
-    }
+    // if (settings().autoanwserquickmaths) {
+    //     setTimeout(() => {
+    //         ChatLib.command('ac ${answer}') 
+    //     }, 200);
+    // }
 }).setCriteria("QUICK MATHS! Solve: ${math}")
