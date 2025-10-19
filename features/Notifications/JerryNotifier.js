1c18800cf7e03b74b236fca8a6be3247dd953836
ChillyPine <chillypines@gmail.com>
2025-10-19 01:18:39 -0600

    Made Jerry Notif not suck

--- features/Notifications/JerryNotifier.js
+++ features/Notifications/JerryNotifier.js
@@ -1,160 +1,31 @@
-//☺ You discovered a color Jerry!
-//☺ There is a color Jerry!
-//☺ You located a hidden color Jerry!
-//☺ A wild color Jerry spawned!
-//☺ Some color Jerry was hiding, but you found it!
-//☺ A color Jerry appeared!
-
 import settings from '../../config'
-const s = settings();
-// const prefix = "&5[Dawn&6Addons&5]";
+const s = settings()
 
+// Map each Jerry type to its color and setting
+const jerries = {
+    golden: { color: "&6", enabled: () => s.goldenJerry },
+    purple: { color: "&5", enabled: () => s.purpleJerry },
+    blue:   { color: "&9", enabled: () => s.blueJerry },
+    green:  { color: "&2", enabled: () => s.greenJerry },
+}
 
-// GOLDEN   
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You discovered a Golden Jerry!")
+// List of all chat message variants
+const messages = [
+    "☺ You discovered a {color} Jerry!",
+    "☺ There is a {color} Jerry!",
+    "☺ You located a hidden {color} Jerry!",
+    "☺ A wild {color} Jerry spawned!",
+    "☺ Some {color} Jerry was hiding, but you found it!",
+    "☺ A {color} Jerry appeared!"
+]
 
-
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ There is a Golden Jerry!")
-
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You located a hidden Golden Jerry!")
-
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A wild Golden Jerry spawned!")
-
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ Some Golden Jerry was hiding, but you found it!")
-
-register("chat", () => {
-    if (!s.goldenJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&6Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A Golden Jerry appeared!")
-
-// PURPLE
-register("chat", () => {   
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You discovered a Purple Jerry!")
-
-register("chat", () => {
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ There is a Purple Jerry!")
-
-register("chat", () => {
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You located a hidden Purple Jerry!")
-
-register("chat", () => {
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A wild Purple Jerry spawned!")
-
-register("chat", () => {
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ Some Purple Jerry was hiding, but you found it!")
-
-register("chat", () => {
-    if (!s.purpleJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&5Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A Purple Jerry appeared!")
-
-// BLUE
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You discovered a Blue Jerry!")
-
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ There is a Blue Jerry!")
-
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You located a hidden Blue Jerry!")
-
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A wild Blue Jerry spawned!")
-
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ Some Blue Jerry was hiding, but you found it!")
-
-register("chat", () => {
-    if (!s.blueJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&9Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A Blue Jerry appeared!")
-
-// GREEN
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You discovered a Green Jerry!")
-
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ There is a Green Jerry!")
-
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ You located a hidden Green Jerry!")
-
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A wild Green Jerry spawned!")
-
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ Some Green Jerry was hiding, but you found it!")
-
-register("chat", () => {
-    if (!s.greenJerry) return
-    if (!s.jerryNotif) return  
-    Client.showTitle("&2Jerry", "", 0, 20, 0);
-}).setCriteria("☺ A Green Jerry appeared!")
+// Register all in one loop
+Object.entries(jerries).forEach(([name, { color, enabled }]) => {
+    messages.forEach(msg => {
+        const criteria = msg.replace("{color}", name.charAt(0).toUpperCase() + name.slice(1))
+        register("chat", () => {
+            if (!enabled() || !s.jerryNotif) return
+            Client.showTitle(`${color}Jerry`, "", 0, 20, 0)
+        }).setCriteria(criteria)
+    })
+})
