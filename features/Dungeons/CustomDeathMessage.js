44e73fc02e43c8f9ce543c353cfaed6d880c35a3
ChillyPine <chillypines@gmail.com>
2025-11-18 11:43:03 -0700

    Remove debug messages from custom death message.

--- features/Dungeons/CustomDeathMessage.js
+++ features/Dungeons/CustomDeathMessage.js
@@ -4,11 +4,8 @@ import { isInDungeon } from "../../utils/Utils";
 
 register('chat', (player) => {
     if (!isInDungeon()) return;
-    ChatLib.chat("check 0")
     player = getIGN(player);
-    ChatLib.chat("Check 1")
     deathMessage = settings().customDeathMessage.replace("{player}", player);
-    ChatLib.chat("Check 2")
     setTimeout(() => {  
         ChatLib.command(`pc ${deathMessage}`);
     }, 300);
