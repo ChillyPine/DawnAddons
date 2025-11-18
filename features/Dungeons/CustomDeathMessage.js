c85ae4ced7aaf90bd6c611df77d6241d1f790fa2
ChillyPine <chillypines@gmail.com>
2025-11-17 23:52:01 -0700

    Fixed custom death message

--- features/Dungeons/CustomDeathMessage.js
+++ features/Dungeons/CustomDeathMessage.js
@@ -1,26 +1,15 @@
 import settings from '../../config'
 import { getIGN } from "../../utils/Function";
-import { checkInWorld } from "../../utils/Location";
-import { registerEventListener } from "../../utils/EventListener";
+import { isInDungeon } from "../../utils/Utils";
 
-registerEventListener(() => settings().customDeathMessage !== "" && checkInWorld("Dungeon"),
-    register("chat", (message) => {
-        const words = message.split(" ");
-
-        const player = getIGN(words[0]);
-        if (player === "You" || player === Player.getName()) return;
-
-        let randomDeathMessage = "";
-        if (settings().customDeathMessage.includes("|")) {
-            const messageArr = settings().customDeathMessage.split("|");
-            randomDeathMessage = messageArr[Math.floor(Math.random() * messageArr.length)].replace("{player}", player);
-        }
-        else {
-            randomDeathMessage = settings().customDeathMessage.replace("{player}", player);
-        }
-
-        setTimeout(() => {
-            ChatLib.command(`pc ${randomDeathMessage}`);
-        }, 300);
-    }).setCriteria(" ☠ ${message} became a ghost.")
-);
+register('chat', (player) => {
+    if (!isInDungeon()) return;
+    ChatLib.chat("check 0")
+    player = getIGN(player);
+    ChatLib.chat("Check 1")
+    deathMessage = settings().customDeathMessage.replace("{player}", player);
+    ChatLib.chat("Check 2")
+    setTimeout(() => {  
+        ChatLib.command(`pc ${deathMessage}`);
+    }, 300);
+}).setCriteria(/^ ☠ (\S+).+/)
